daff3b867f9e34c07c6e335d54045429d0502483
greenhat616 <i@a632079.me>
2023-10-22 17:37:35 +0800

    fix: stylelint issues

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,26 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-html/vue'
+  ],
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties'
+  ],
+  ignoreFiles: [
+    'node_modules/**/*',
+    'dist/**/*',
+    '**/typings/**/*',
+    'public/css/**/*'
+  ],
+  overrides: [
+    {
+      files: ['**/*.scss', '*.scss'],
+      customSyntax: require('postcss-scss')
+    }
+  ]
 }
